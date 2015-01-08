374845f1899d8a6f1eb2daa125418ac3ab004215
Max Petyurenko <max@drucode.com>
2015-01-08 11:58:11 +0200

    Custom styles. Grid fix.

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,2 +1,3 @@
 class Node < ActiveRecord::Base
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  has_and_belongs_to_many :nodes
+  has_many :nodes
 
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
 
