038f94fb1889b39e4ec486d81c0e815fe61500bf
Max Petyurenko <max@drucode.com>
2015-02-02 17:47:04 +0200

    Front menu. Smooth scroll.

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -27,3 +27,30 @@ function drawChart(data, options) {
   var chart = new google.charts.Bar(document.getElementById('skills-chart'));
   chart.draw(data, options);
 }
+
+(function($) {
+  $(window).load(function(){
+    $(window).scroll(function() {
+      var offset = $(this).scrollTop();
+      if (offset >= 400) {
+        $('nav.navbar').addClass('background');
+      }
+      else if (offset < 400) {
+        $('nav.navbar').removeClass('background');
+      }
+    });
+
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 50
+          }, 700);
+          return false;
+        }
+      }
+    });
+  });
+})(jQuery);
