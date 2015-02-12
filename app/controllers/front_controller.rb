a9d7bd0ebc1a1a6b474ec1d8e8a4dc504017a59c
Max Petyurenko <max@drucode.com>
2015-02-12 13:59:43 +0200

    Front controller refactoring.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -6,58 +6,20 @@ class FrontController < ApplicationController
     @user = User.find(1)
     @profile = @user.profile
 
-    # Collect social.
-    @social = {}
-
-    if !@profile.nil?
-      social = ['github', 'drupal', 'linkedin']
-      social.each do |v|
-        if !@profile[v].nil? && !@profile[v].empty?
-          @social[v] = @profile[v]
-        end
-      end
-    end
+    # Collect social links.
+    @social = get_social_links(@profile)
 
     # Get Gravatar image.
-    @profile_image = gimage(@user.email, '200')
+    @profile_image = get_gimage(@user.email, '200')
 
     # Nodes list.
     nodes = Node.published.to_ary
     @nodes = nodes.each_slice(2).to_a
 
-    # List of skills for bar chart.
-    skills = {
-      'Skill' => 'Skill',
-      'Drupal' => 8,
-      'PHP' => 8,
-      'SQL' => 8,
-      'jQuery' => 7,
-      'jQuery UI' => 6,
-      'CSS' => 7,
-      'HTML' => 7,
-      'AJAX' => 7,
-      'Git' => 7,
-      'Apache' => 3,
-      'Solr' => 2,
-      'Github' => 2,
-      'JIRA' => 3,
-      'Confluence' => 3,
-      'Scrum' => 3,
-      'Kanban' => 1,
-      'Intellij IDEA' => 1,
-      'Redmine' => 1,
-      'Basecamp' => 1,
-      'Tortoise SVN' => 1,
-      'Web Development' => 1,
-      'JavaScript' => 1,
-      'MySQL' => 1,
-      'HTML5' => 0,
-    }
-
-    gon.skills_keys = skills.keys
-    gon.skills_values = skills.values
+    # Pass skills variables to gon.
+    set_skills_data
 
     # Get github repositories.
-    @repos = github_repos('paravibe')
+    @repos = get_github_repos('paravibe')
   end
 end
--- app/helpers/front_helper.rb
+++ app/helpers/front_helper.rb
@@ -1,12 +1,12 @@
 module FrontHelper
-  def gimage (email, size = '100')
+  def get_gimage (email, size = '100')
     hash = Digest::MD5.hexdigest(email.downcase)
     image_url = 'http://www.gravatar.com/avatar/' + hash
     image_url += '?s=' + size + '&d=mm'
     return image_url
   end
 
-  def github_repos (user)
+  def get_github_repos (user)
     repositories = []
 
     github = Github.new
@@ -21,5 +21,55 @@ module FrontHelper
     end
 
     return repositories
+  end
+
+  def set_skills_data
+    # List of skills for bar chart.
+    skills = {
+      'Skill' => 'Skill',
+      'Drupal' => 8,
+      'PHP' => 8,
+      'SQL' => 8,
+      'jQuery' => 7,
+      'jQuery UI' => 6,
+      'CSS' => 7,
+      'HTML' => 7,
+      'AJAX' => 7,
+      'Git' => 7,
+      'Apache' => 3,
+      'Solr' => 2,
+      'Github' => 2,
+      'JIRA' => 3,
+      'Confluence' => 3,
+      'Scrum' => 3,
+      'Kanban' => 1,
+      'Intellij IDEA' => 1,
+      'Redmine' => 1,
+      'Basecamp' => 1,
+      'Tortoise SVN' => 1,
+      'Web Development' => 1,
+      'JavaScript' => 1,
+      'MySQL' => 1,
+      'HTML5' => 0,
+    }
+
+    # Set variables to page.
+    gon.skills_keys = skills.keys
+    gon.skills_values = skills.values
+  end
+
+  def get_social_links(profile)
+    links = {}
+
+    if !profile.nil?
+      social = ['github', 'drupal', 'linkedin']
+      social.each do |v|
+        if !profile[v].nil? && !profile[v].empty?
+          links[v] = profile[v]
+        end
+      end
+    end
+
+    return links
   end
 end
