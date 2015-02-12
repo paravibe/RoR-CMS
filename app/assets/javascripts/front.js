e133a88ba1d64990902d5fe8a337abb0bbf71cf9
Max Petyurenko <max@drucode.com>
2015-02-12 10:01:52 +0200

    Front menu. Styles update.

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -44,9 +44,12 @@ function drawChart(data, options) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top - 50
-          }, 700);
+          $('html,body').animate(
+            {
+              scrollTop: target.offset().top - 50
+            },
+            700
+          );
           return false;
         }
       }
